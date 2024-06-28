398e740f46d17b9ef6f4454e6f1599a2f053ed89
Srikruth <s.puram@ufl.edu>
2024-06-28 01:52:25 -0400

    committing changes with new animations

--- src/App.js
+++ src/App.js
@@ -5,28 +5,32 @@ import Home from './components/Home';
 import About from './components/About';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 import './App.scss';
 import { motion, AnimatePresence } from 'framer-motion';
+import Loader from 'react-loaders';
 
 const shrinkZoomVariants = {
   initial: {
     opacity: 0,
-    scale: 0.8, // Start slightly smaller
+    scale: 0.3,
   },
   in: {
     opacity: 1,
-    scale: 1, // Normal size
+    scale: 1,
     transition: {
-      duration: 0.3, // Adjust duration as needed
-      ease: 'easeInOut', // Use easeInOut for smoother effect
+      delay: 1.2, // Maintain the delay for smoother effect
+      type: 'spring',
+      stiffness: 150, // Lower stiffness for smoother animation
+      damping: 30, // Higher damping for less bounce
     },
   },
   out: {
     opacity: 0,
-    scale: 0.8, // Shrink to smaller size
+    scale: 0.3,
     transition: {
-      duration: 0.3,
-      ease: 'easeInOut',
+      duration: 0.5,
+      ease: [0.42, 0, 0.58, 1],
     },
   },
 };
@@ -41,61 +45,91 @@ function App() {
           <Route
             index
             element={
-              <motion.div
-                key="home"
-                className="animated-route"
-                variants={shrinkZoomVariants}
-                initial="initial"
-                animate="in"
-                exit="out"
-              >
-                <Home location={location} />
-              </motion.div>
+              <>
+                <motion.div
+                  key="home"
+                  className="animated-route"
+                  variants={shrinkZoomVariants}
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                >
+                  <Home location={location} />
+                </motion.div>
+                <Loader type="pacman" className="loader-active" />
+              </>
             }
           />
           <Route
             path="about"
             element={
-              <motion.div
-                key="about"
-                className="animated-route"
-                variants={shrinkZoomVariants}
-                initial="initial"
-                animate="in"
-                exit="out"
-              >
-                <About />
-              </motion.div>
+              <>
+                <motion.div
+                  key="about"
+                  className="animated-route"
+                  variants={shrinkZoomVariants}
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                >
+                  <About />
+                </motion.div>
+                <Loader type="pacman" className="loader-active" />
+              </>
             }
           />
           <Route
             path="experience"
             element={
-              <motion.div
-                key="experience"
-                className="animated-route"
-                variants={shrinkZoomVariants}
-                initial="initial"
-                animate="in"
-                exit="out"
-              >
-                <Experience />
-              </motion.div>
+              <>
+                <motion.div
+                  key="experience"
+                  className="animated-route"
+                  variants={shrinkZoomVariants}
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                >
+                  <Experience />
+                </motion.div>
+                <Loader type="pacman" className="loader-active" />
+              </>
             }
           />
           <Route
             path="projects"
             element={
-              <motion.div
-                key="projects"
-                className="animated-route"
-                variants={shrinkZoomVariants}
-                initial="initial"
-                animate="in"
-                exit="out"
-              >
-                <Projects />
-              </motion.div>
+              <>
+                <motion.div
+                  key="projects"
+                  className="animated-route"
+                  variants={shrinkZoomVariants}
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                >
+                  <Projects />
+                </motion.div>
+                <Loader type="pacman" className="loader-active" />
+              </>
+            }
+          />
+          <Route
+            path="contact"
+            element={
+              <>
+                <motion.div
+                  key="contact"
+                  className="animated-route"
+                  variants={shrinkZoomVariants}
+                  initial="initial"
+                  animate="in"
+                  exit="out"
+                >
+                  <Contact />
+                </motion.div>
+                <Loader type="pacman" className="loader-active" />
+              </>
             }
           />
         </Route>
---
+++ src/components/Contact/index.js
@@ -0,0 +1,96 @@
+import { useEffect, useState, useRef } from 'react';
+import emailjs from 'emailjs-com'; // Correct import statement
+import AnimatedLetters from '../AnimatedLetters';
+import './index.scss';
+
+const Contact = () => {
+  console.log(process.env.REACT_APP_EMAILJS_SERVICE_ID);
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const form = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+  }, []);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        form.current,
+        process.env.REACT_APP_EMAILJS_USER_ID
+      )
+      .then(
+        () => {
+          alert('Message successfully sent!');
+          window.location.reload(false);
+        },
+        () => {
+          alert('Failed to send the message, please try again');
+        }
+      );
+  };
+
+  return (
+    <>
+      <div className="container contact-page">
+        <div className="contact-textzone text-zone">
+          <h1>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={['C', 'o', 'n', 't', 'a', 'c', 't', ' ', 'm', 'e']}
+              idx={15}
+            />
+          </h1>
+          <p>
+            I am interested in full-time and freelance opportunities -
+            especially on ambitious or large projects. However, if you have any
+            other requests or questions, don't hesitate to contact me using
+            below form either.
+          </p>
+          <div className="contact-form">
+            <form ref={form} onSubmit={sendEmail}>
+              <ul>
+                <li className="half">
+                  <input placeholder="Name" type="text" name="name" required />
+                </li>
+                <li className="half">
+                  <input
+                    placeholder="Email"
+                    type="email"
+                    name="email"
+                    required
+                  />
+                </li>
+                <li>
+                  <input
+                    placeholder="Subject"
+                    type="text"
+                    name="subject"
+                    required
+                  />
+                </li>
+                <li>
+                  <textarea
+                    placeholder="Message"
+                    name="message"
+                    required
+                  ></textarea>
+                </li>
+                <li>
+                  <input type="submit" className="flat-button" value="SEND" />
+                </li>
+              </ul>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
